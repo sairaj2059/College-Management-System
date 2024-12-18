78706bd62fc1513c76be4fbd13107b16c6ef57ea
sairaj2059 <rajguragai124@gmail.com>
2024-12-18 17:00:52 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Working from './Title';
+
+function App() {
+  return (
+    <div className="App">
+      <Working/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Working() {
+  return (
+    <>
+      <div>Final Year Project</div>
+      <p>By Raj, Pravesh And Shyam</p>
+    </>
+  );
+}
+
+export default Working;
