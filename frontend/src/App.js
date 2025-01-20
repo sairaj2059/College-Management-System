01505e198320535849776b96bc1579a5f1ce1541
Pravesh Subba <praveshsubba81@gmail.com>
2025-01-21 02:45:53 +0530

    Added favicon and removed unsed logos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 import Working from './Title';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Working/>
     </div>
   );
--- frontend/src/Title.jsx
+++ frontend/src/Title.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Working() {
+const Working = () => {
   return (
     <>
       <div>Final Year Project</div>
