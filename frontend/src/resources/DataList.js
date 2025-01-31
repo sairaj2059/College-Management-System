ef14880d2b6d9bb7abdf5204e363b20d78d5dd8e
sairaj2059 <rajguragai124@gmail.com>
2025-01-31 19:56:02 +0530

    Added Student Dashboard

---
+++ frontend/src/resources/DataList.js
@@ -0,0 +1,11 @@
+import SyamImage from "../resources/images/224206.jpg"
+
+export const UserData = 
+    {
+        name: "Kokkonda Sri Shyam Sundar",
+        regdNo: "224206",
+        course: "BSc CS",
+        year: "III",
+        semester: "6",
+        image: SyamImage
+    }
