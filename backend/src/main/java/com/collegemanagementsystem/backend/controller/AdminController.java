6614ca7158d7d88bacb8e7f69024382993ee609f
shyamsunder-005 <113104070+shyamsunder-005@users.noreply.github.com>
2025-02-04 16:28:28 +0530

    Update AdminController.java

--- backend/src/main/java/com/collegemanagementsystem/backend/controller/AdminController.java
+++ backend/src/main/java/com/collegemanagementsystem/backend/controller/AdminController.java
@@ -4,27 +4,25 @@ import com.collegemanagementsystem.backend.dto.LoginRequest;
 import com.collegemanagementsystem.backend.model.AuthResponse;
 import com.collegemanagementsystem.backend.model.UserAuth;
 import com.collegemanagementsystem.backend.service.AdminService;
+import com.collegemanagementsystem.backend.service.JWTService;
 import jakarta.servlet.http.HttpServletRequest;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @CrossOrigin(origins = "*")
 public class AdminController {
+
     @Autowired
     private AdminService service;
-
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("/dashboard")
 
-    public String greet(HttpServletRequest request) {
-        return "Hello Admin" + request.getSession().getId();
+    public String greet(HttpServletRequest request){
+        return "Hello Admin"+request.getSession().getId();
     }
 
     @PostMapping("/register")
@@ -33,23 +31,19 @@ public class AdminController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> login(
-            @RequestBody LoginRequest loginRequest) {
-        String username = loginRequest.getUsername();
-        String password = loginRequest.getPassword();
-
-        Map<String, Object> response = new HashMap<>();
-        AuthResponse authenticate = service.verify(username, password);
-        if (authenticate.getJwtToken() != "fail") {
+    public ResponseEntity<Map<String, Object>> login(@RequestBody UserAuth student){
+        Map<String,Object> response = new HashMap<>();
+        String authenticate = service.verify(student);
+        if (authenticate != "fail"){
             response.put("success", true);
-            response.put("token", authenticate.getJwtToken());
-            response.put("role", authenticate.getUserAuth().getRole());
+            response.put("token", authenticate);
             return ResponseEntity.ok(response);
         } else {
             response.put("success", false);
             response.put("token", "Invalid Credentials");
             return ResponseEntity.status(404).body(response);
         }
-
+        
+        
     }
 }
