05e5fd79639db747c61fd256dbdf195e8b5d83f1
sairaj2059 <rajguragai124@gmail.com>
2025-02-06 20:46:28 +0530

    added Piechart

--- backend/src/main/java/com/collegemanagementsystem/backend/controller/StudentController.java
+++ backend/src/main/java/com/collegemanagementsystem/backend/controller/StudentController.java
@@ -1,43 +1,41 @@
-// package com.collegemanagementsystem.backend.controller;
+package com.collegemanagementsystem.backend.controller;
 
-// import java.time.YearMonth;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import com.collegemanagementsystem.backend.model.ClasswiseAttendance;
 
-// import com.collegemanagementsystem.backend.model.ClasswiseAttendance;
-// import com.collegemanagementsystem.backend.model.ClasswiseAttendance.Student;
-// import com.collegemanagementsystem.backend.model.ClasswiseAttendance.Student.AttendanceMonth;
-// import com.collegemanagementsystem.backend.service.StudentService;
+import com.collegemanagementsystem.backend.model.ClasswiseAttendance.Student.AttendanceMonth;
+import com.collegemanagementsystem.backend.service.StudentService;
 
-// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 
-// @RestController
-// @RequestMapping("/student")
-// public class StudentController {
+@RestController
+@RequestMapping("/student")
+public class StudentController {
 
-//     @Autowired
-//     private StudentService studentService;
+    @Autowired
+    private StudentService studentService;
 
-//     @GetMapping("/dashboard")
-//     public String greetByStudent() {
-//         return "Hello Student from Student Dashboard";
-//     }
+    @GetMapping("/dashboard")
+    public String greetByStudent() {
+        return "Hello Student from Student Dashboard";
+    }
 
-//     @GetMapping("/get_attendance")
-//     public ResponseEntity<AttendanceMonth> getAttendanceMonth(
-//             @RequestParam String className,
-//             @RequestParam String regdNo,
-//             @RequestParam String month) {
+    @GetMapping("/get_attendance")
+    public ResponseEntity<AttendanceMonth> getAttendanceMonth(
+            @RequestParam String className,
+            @RequestParam String regdNo,
+            @RequestParam String month) {
 
-//         System.out.println("classname:" + className);
-//         System.out.println("regdNo:" + regdNo);
-//         System.out.println("month:" + month);
-//         ClasswiseAttendance attendanceMonth = studentService.getStudentAttendanceByMonth(className, regdNo, month);
-//         return ResponseEntity.ok(attendanceMonth.getStudents().get(0).getAttendance().get(0));
-//     }
+        System.out.println("classname:" + className);
+        System.out.println("regdNo:" + regdNo);
+        System.out.println("month:" + month);
+        ClasswiseAttendance attendanceMonth = studentService.getStudentAttendanceByMonth(className, regdNo, month);
+        return ResponseEntity.ok(attendanceMonth.getStudents().get(0).getAttendance().get(0));
+    }
 
-// }
+}
--- frontend/src/pages/StudentDashboard.jsx
+++ frontend/src/pages/StudentDashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AspectRatio from "@mui/joy/AspectRatio";
 import Box from "@mui/joy/Box";
 import Card from "@mui/joy/Card";
@@ -31,12 +31,31 @@ import ListItemContent from "@mui/joy/ListItemContent";
 import Avatar from "@mui/joy/Avatar";
 import Input from "@mui/joy/Input";
 import Button from "@mui/joy/Button";
+import { BarChart, legendClasses, PieChart } from "@mui/x-charts";
 
+const otherProps = {
+  width: 400,
+  height: 200,
+  sx: {
+    [`.${legendClasses.root}`]: {
+      transform: 'translate(20px, 0)',
+    },
+  },
+};
+
+const data = [
+  { team: 'Amber Ants', rank: 3, points: 31 },
+  { team: 'Eagle Warriors', rank: 1, points: 50 },
+  { team: 'Elephant Trunk', rank: 4, points: 18 },
+  { team: 'Jaguars', rank: 2, points: 37 },
+  { team: 'Smooth Pandas', rank: 5, points: 6 },
+];
 function UserCard() {
+
   return (
     <Box
       sx={{
-        width: "35%",
+        width: "30vw",
         position: "relative",
         overflow: { xs: "auto", sm: "initial" },
         mt: "2%",
@@ -73,10 +92,8 @@ function UserCard() {
           <Typography
             level="body-sm"
             textColor="text.tertiary"
-            variant="solid"
-            color="primary"
             noWrap
-            sx={{ fontWeight: "lg" }}
+            sx={{ fontWeight: "lg", display: "inline-block" }}
           >
             #{UserData.regdNo}
           </Typography>
@@ -134,6 +151,14 @@ function UserCard() {
 // }
 
 function StudentDashboard() {
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  
+  
   return (
     <>
       <CssVarsProvider>
@@ -149,27 +174,32 @@ function StudentDashboard() {
               // backgroundColor:'#004071',
               borderRight: "1px solid",
               borderColor: "divider",
-              width: "20vw",
+              width: isExpanded ? "25vw" : "5vw",
               height: "100vh",
               display: "flex",
               flexDirection: "column",
+              overflow: "hidden",
             }}
           >
             <Box
               className="sidebar-header"
               sx={{
-                p: "5%",
+                m: "5%",
                 display: "flex",
                 gap: 1,
                 alignItems: "center",
-                justifyContent: "flex-start",
+                justifyContent: "space-between",
               }}
             >
-              <IconButton variant="soft" color="black" size="sm">
-                <Avatar src={sssihlLogo} />
-              </IconButton>
-              <Typography level="title-lg">Nandigiri Campus</Typography>
-              <IconButton>
+              {isExpanded && (
+                <IconButton variant="soft" color="black" size="sm">
+                  <Avatar src={sssihlLogo} />
+                </IconButton>
+              )}
+              {isExpanded && (
+                <Typography level="title-lg">Nandigiri Campus</Typography>
+              )}
+              <IconButton onClick={toggleSidebar} sx={{ marginLeft: "auto" }}>
                 <MenuRoundedIcon sx={{ fontSize: 25 }} />
               </IconButton>
             </Box>
@@ -183,7 +213,7 @@ function StudentDashboard() {
             <List sx={{ mt: "2%", alignSelf: "center", width: "90%" }}>
               <ListItem sx={{ m: "3%", p: "3%" }}>
                 <ListItemButton sx={{ borderRadius: 7 }}>
-                  <HomeRoundedIcon />
+                  <HomeRoundedIcon expanded={isExpanded} />
                   <ListItemContent>
                     <Typography level="title-sm">Home</Typography>
                   </ListItemContent>
@@ -238,8 +268,24 @@ function StudentDashboard() {
                 </ListItemButton>
               </ListItem>
             </List>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                marginInline: "3%",
+                paddingInline: "2%",
+              }}
+            >
+              <Avatar variant="outlined" size="md" src={UserData.image} />
+              <Box sx={{ m: "3%", p: "3%" }}>
+                <Typography level="title-sm">{UserData.name}</Typography>
+                <Typography level="body-xs">{UserData.course}</Typography>
+              </Box>
+              <IconButton size="sm" variant="plain" color="neutral">
+                <LogoutRoundedIcon />
+              </IconButton>
+            </Box>
           </Box>
-          {/* <Sidebar /> */}
           <Box
             component="main"
             className="main-container"
@@ -260,11 +306,11 @@ function StudentDashboard() {
                   backgroundColor: "#ecebeb",
                   color: "black",
                   borderBottom: "2px solid #fbe855",
-                  padding:'1%',
-                  width:'10vw',
-                  ":hover":{
-                    backgroundColor:'#e2e1e1'
-                  }
+                  padding: "1%",
+                  width: "10vw",
+                  ":hover": {
+                    backgroundColor: "#e2e1e1",
+                  },
                 }}
                 startDecorator={<AssignmentRoundedIcon />}
               >
@@ -275,11 +321,11 @@ function StudentDashboard() {
                   backgroundColor: "#ecebeb",
                   color: "black",
                   borderBottom: "2px solid #4a4afd",
-                  padding:'1%',
-                  width:'10vw',
-                  ":hover":{
-                    backgroundColor:'#e2e1e1'
-                  }
+                  padding: "1%",
+                  width: "10vw",
+                  ":hover": {
+                    backgroundColor: "#e2e1e1",
+                  },
                 }}
                 startDecorator={<CalendarTodayRounded />}
               >
@@ -290,16 +336,32 @@ function StudentDashboard() {
                   backgroundColor: "#ecebeb",
                   color: "black",
                   borderBottom: "2px solid #fe4f4f",
-                  padding:'1%',
-                  width:'10vw',
-                  ":hover":{
-                    backgroundColor:'#e2e1e1'
-                  }
+                  padding: "1%",
+                  width: "10vw",
+                  ":hover": {
+                    backgroundColor: "#e2e1e1",
+                  },
                 }}
                 startDecorator={<CalendarMonthRounded />}
               >
                 Calendar
               </Button>
+            </Box>
+            <Box>
+
+<PieChart
+  series={[
+    {
+      data: [
+        { id: 0, value: 10, label: 'series A' },
+        { id: 1, value: 15, label: 'series B' },
+        { id: 2, value: 20, label: 'series C' },
+      ],
+    },
+  ]}
+  width={400}
+  height={200}
+/>
             </Box>
           </Box>
         </Box>
