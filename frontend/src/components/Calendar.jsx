751210867c27f47b9d9a9fb2fc7ca6661a04bf69
shyamsunder-005 <sreeshyamasunderkokkonda@gmail.com>
2025-02-15 20:36:29 +0530

    fixed package

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { Card, CardContent, Typography } from "@mui/material";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Calendar = () => {
   const [date, setDate] = useState(new Date());
@@ -11,9 +10,12 @@ const Calendar = () => {
     <Card sx={{ width: 400, p: 2, borderRadius: 3, boxShadow: 3 }}>
       <CardContent>
         <Typography variant="h6">Schedules</Typography>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <DateCalendar value={date} onChange={(newDate) => setDate(newDate)} />
-        </LocalizationProvider>
+        <DatePicker 
+          selected={date} 
+          onChange={(newDate) => setDate(newDate)} 
+          dateFormat="dd/MM/yyyy"
+          className="custom-datepicker"
+        />
       </CardContent>
     </Card>
   );
