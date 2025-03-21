09abaacabff80e2d75cd4a56a007c34ea979bd87
Pravesh Subba <praveshsubba81@gmail.com>
2025-03-21 12:12:52 +0530

    notice component completed

--- backend/src/main/java/com/collegemanagementsystem/backend/controller/AdminController.java
+++ backend/src/main/java/com/collegemanagementsystem/backend/controller/AdminController.java
@@ -7,7 +7,6 @@ import com.collegemanagementsystem.backend.service.AdministratorService;
 import com.collegemanagementsystem.backend.service.NoticeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
