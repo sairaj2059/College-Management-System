93de1a787cd517411630522631e79259534ea836
shyamsunder-005 <sreeshyamasunderkokkonda@gmail.com>
2025-04-03 11:30:09 +0530

    Change Password is done!!!!

--- frontend/src/components/DropdownComponent.jsx
+++ frontend/src/components/DropdownComponent.jsx
@@ -1,11 +1,7 @@
-import React from "react";
-import { Dropdown, Space, Typography } from "antd";
+import React, { useState } from "react";
+import { Modal, Form, Input, Button, message, Dropdown, Space, Typography } from "antd";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import {
-  UserOutlined,
-  SettingOutlined,
-  LogoutOutlined,
-} from "@ant-design/icons";
+import { UserOutlined, SettingOutlined, LogoutOutlined, KeyOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../redux/slices/authSlice";
 import { useDispatch } from "react-redux";
@@ -13,6 +9,9 @@ import { useDispatch } from "react-redux";
 export const DropdownComponent = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [form] = Form.useForm();
 
   const handleNavigation = (path) => {
     navigate(path);
@@ -21,13 +20,57 @@ export const DropdownComponent = () => {
   const logOut = () => {
     dispatch(logout());
     navigate("/login");
-  }
+  };
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+    form.resetFields();
+  };
+
+  const handlePasswordChange = async () => {
+    try {
+      const values = await form.validateFields();
+      setLoading(true);
+
+      const response = await fetch("http://localhost:8080/change-password", {
+        method: "POST",
+        headers: { 
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${localStorage.getItem("token")}` // Include token if needed
+        },
+        body: JSON.stringify({
+          username: localStorage.getItem("username"), // Use logged-in user's username
+          
+          oldPassword: values.oldPassword,
+          newPassword: values.newPassword,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        message.success("Password updated successfully!");
+        handleCloseModal();
+      } else {
+        message.error("Invalid old password.");
+      }
+    } catch (error) {
+      console.error("Error updating password:", error);
+      message.error("An error occurred.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const items = [
     {
       label: (
         <div
-          onClick={() => handleNavigation("/")}
+          onClick={showModal}
           style={{
             display: "flex",
             alignItems: "center",
@@ -35,8 +78,8 @@ export const DropdownComponent = () => {
             justifyContent: "space-between",
           }}
         >
-          <UserOutlined />
-          <Typography>Edit profile</Typography>
+          <KeyOutlined />
+          <Typography>Change Password</Typography>
         </div>
       ),
       key: "0",
@@ -44,10 +87,10 @@ export const DropdownComponent = () => {
     {
       label: (
         <div
-          onClick={() => handleNavigation("/")}
+          onClick={() => handleNavigation("/settings")}
           style={{
             display: "flex",
-            alignItems: "right",
+            alignItems: "center",
             flexDirection: "row",
             justifyContent: "space-between",
           }}
@@ -81,18 +124,70 @@ export const DropdownComponent = () => {
   ];
 
   return (
-    <Dropdown
-      menu={{
-        items,
-      }}
-      trigger={["click"]}
-      overlayStyle={{ minWidth: 130, minHeigh: 160, textAlign: "right" }}
-    >
-      <span style={{ cursor: "pointer" }}>
-        <Space>
-          <ExpandMoreIcon  sx={{ color: "white" }}/>
-        </Space>
-      </span>
-    </Dropdown>
+    <>
+      {/* Dropdown Menu */}
+      <Dropdown menu={{ items }} trigger={["click"]} overlayStyle={{ minWidth: 130, textAlign: "right" }}>
+        <span style={{ cursor: "pointer" }}>
+          <Space>
+            <ExpandMoreIcon sx={{ color: "white" }} />
+          </Space>
+        </span>
+      </Dropdown>
+
+      {/* Change Password Modal */}
+      <Modal
+        title="Change Password"
+        visible={isModalVisible}
+        onCancel={handleCloseModal}
+        footer={[
+          <Button key="cancel" onClick={handleCloseModal}>
+            Cancel
+          </Button>,
+          <Button key="submit" type="primary" loading={loading} onClick={handlePasswordChange}>
+            Change Password
+          </Button>,
+        ]}
+      >
+        <Form form={form} layout="vertical">
+          <Form.Item
+            name="oldPassword"
+            label="Current Password"
+            rules={[{ required: true, message: "Please enter your current password!" }]}
+          >
+            <Input.Password placeholder="Enter current password" />
+          </Form.Item>
+
+          <Form.Item
+            name="newPassword"
+            label="New Password"
+            rules={[
+              { required: true, message: "Please enter a new password!" },
+              { min: 6, message: "Password must be at least 6 characters!" },
+            ]}
+          >
+            <Input.Password placeholder="Enter new password" />
+          </Form.Item>
+
+          <Form.Item
+            name="confirmPassword"
+            label="Confirm New Password"
+            dependencies={["newPassword"]}
+            rules={[
+              { required: true, message: "Please confirm your new password!" },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("newPassword") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error("Passwords do not match!"));
+                },
+              }),
+            ]}
+          >
+            <Input.Password placeholder="Confirm new password" />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
   );
 };
